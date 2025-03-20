42eb6ea4a2fa510bf791d3aed89b075dfe333dec
ldesfontaine <lucas.desfontaine1@gmail.com>
2025-03-21 09:08:31 +1100

    fix

--- tp2/exo2.java
+++ tp2/exo2.java
@@ -1,19 +1,8 @@
 package tp2;
 
-/*
-Dans le plan, un point est représenté par ses coordonnées : x = abscisse, y = ordonnée.
-Ecrire une classe Point qui contienne :
-    - un constructeur
-    - une méthode permettant de translater un point d’une valeur dx en x et dy en y
-    - une méthode toString permettant d’afficher les coordonnés d’un point sous la forme (x, y)
-    - une méthode main permettant de tester les ressources de la classe Point.
-    N.B. : Les attributs d’un point seront définis comme étant privés.
- */
-
 
 public class exo2 {
 
-    //    create class Point
     public static class Point {
         private double x;
         private double y;
